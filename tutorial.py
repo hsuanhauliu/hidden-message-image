f8a41f717ad7d9b0d738950f70b74ff15afe6cc7
hsuanhauliu <howardliu43Gmail.com>
2018-06-07 23:14:47 -0700

    changed name

--- tutorial.py
+++ tutorial.py
@@ -1,17 +1,17 @@
-import IHM
+import HMI
 
 # User inputs
 inputFile = "pics/test1.jpg"
 inputStr = "You can't see me"
 
 # Create a IHM class to start
-ihm = IHM.IHM()
+hmi = HMI.HMI()
 
 # Encrypt the message by inputting a image and a string
-ihm.encrypt(inputStr, inputFile)
+hmi.encrypt(inputStr, inputFile)
 
 # Use the original image and encrypted version to see the hidden message
-ihm.decrypt(inputFile, "output.png")
+hmi.decrypt(inputFile, "output.png")
 
 # Note that the order of inputs doesn't matter
-ihm.decrypt("output.png", inputFile)
+hmi.decrypt("output.png", inputFile)
